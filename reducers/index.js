77e788e731fbc26318890df63736e14ffa1ce56d
gumennii <mihail.gumennii@gmail.com>
2017-11-20 20:57:20 -0800

    feat: add placeholders actions and reducer

---
+++ reducers/index.js
@@ -0,0 +1,13 @@
+import {
+  QUIZ_START,
+  QUESTION_ADD,
+  QUESTION_ANSWERED_CORRECT,
+  QUESTION_ANSWERED_INCORRECT,
+  DECK_CREATE
+}
+
+function decks (state = {}, action) => {
+  switch(action) {
+    default: return state
+  }
+}
