68ee8a47b4284b282052e64738bc2d512825f7ea
gumennii <mihail.gumennii@gmail.com>
2017-11-21 21:12:43 -0800

    feat: create deck list markup

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,16 +1,51 @@
 import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, FlatList} from 'react-native'
 
-const DeckList = (props) => {
+const DeckCard = ({ card, navigation }) => {
   return (
-    <View>
+    <View style={styles.deck}>
       <TouchableOpacity onPress={() => navigation.navigate('Deck')}>
-        <Text>{JSON.stringify(props)}</Text>
+        <Text style={styles.title}>{card.title}</Text>
+        <Text style={styles.amount}>{card.questions.length}</Text>
       </TouchableOpacity>
     </View>
-  );
+  )
 }
+
+const DeckList = ({ decks, navigation }) => {
+  return (
+    <View style={styles.list}>
+      <FlatList
+          data={Object.keys(decks)}
+          renderItem={(card) => <DeckCard card={decks[card.item]} navigation={navigation}/>}
+        />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  list: {
+    flex: 1,
+    padding: 15,
+    backgroundColor: '#FFF'
+  },
+  deck: {
+    padding: 15,
+    marginBottom: 15,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#d6d7da'
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+  amount: {
+    textAlign: 'center'
+  }
+})
 
 const mapStateToProps = (state) => {
   return {
