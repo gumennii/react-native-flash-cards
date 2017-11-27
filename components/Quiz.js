5a1dfff6fcc2e73b2782b23ae52a52545d8be0bd
gumennii <mihail.gumennii@gmail.com>
2017-11-26 17:00:38 -0800

    refactor: add ternary operator

--- components/Quiz.js
+++ components/Quiz.js
@@ -58,56 +58,52 @@ class Quiz extends Component {
     let question = this.props.questions[this.state.current].question
     let answer = this.props.questions[this.state.current].answer
 
-    if (this.state.showResults === false) {
-      return (
-        <View style={styles.container}>
-          <View>
-            <Text style={styles.cardsLeft}>{`Card ${this.state.current + 1} of ${this.props.questions.length}`}</Text>
-          </View>
-          <View style={styles.content}>
-            <View style={styles.deck}>
-              <Text style={styles.title}>
-                {this.state.showQuestion ? question : answer}
-              </Text>
-              <TouchableOpacity onPress={() => this.toggleView()}>
-                <Text style={styles.toggle}>{this.state.showQuestion ? 'Show Answer' : 'Show Question'}</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-          <View style={styles.footer}>
-            <TouchableOpacity
-              style={[styles.button, {backgroundColor:'#7AC74F', marginBottom: 10}]}
-              onPress={() => this.submitAnswer('correct')}>
-              <Text style={styles.buttonText}>Correct</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={[styles.button, {backgroundColor:'#DB504A'}]}
-              onPress={() => this.submitAnswer('incorrect')}>
-              <Text style={styles.buttonText}>Incorrect</Text>
+    return (this.state.showResults === false) ? (
+      <View style={styles.container}>
+        <View>
+          <Text style={styles.cardsLeft}>{`Card ${this.state.current + 1} of ${this.props.questions.length}`}</Text>
+        </View>
+        <View style={styles.content}>
+          <View style={styles.deck}>
+            <Text style={styles.title}>
+              {this.state.showQuestion ? question : answer}
+            </Text>
+            <TouchableOpacity onPress={() => this.toggleView()}>
+              <Text style={styles.toggle}>{this.state.showQuestion ? 'Show Answer' : 'Show Question'}</Text>
             </TouchableOpacity>
           </View>
         </View>
-      )
-    } else {
-      return (
-        <View style={styles.container}>
-          <View style={styles.content}>
-            <View style={styles.deck}>
-              <Text style={styles.title}>
-                Your Result is {this.calculatePrecentage()}
-              </Text>
-            </View>
-          </View>
-          <View style={styles.footer}>
-            <TouchableOpacity
-              style={[styles.button, {backgroundColor:'#000'}]}
-              onPress={() => this.goToDeckList()}>
-              <Text style={[styles.buttonText, {color: '#FFF'}]}>Go Home</Text>
-            </TouchableOpacity>
+        <View style={styles.footer}>
+          <TouchableOpacity
+            style={[styles.button, {backgroundColor:'#7AC74F', marginBottom: 10}]}
+            onPress={() => this.submitAnswer('correct')}>
+            <Text style={styles.buttonText}>Correct</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.button, {backgroundColor:'#DB504A'}]}
+            onPress={() => this.submitAnswer('incorrect')}>
+            <Text style={styles.buttonText}>Incorrect</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    ) : (
+      <View style={styles.container}>
+        <View style={styles.content}>
+          <View style={styles.deck}>
+            <Text style={styles.title}>
+              Your Result is {this.calculatePrecentage()}
+            </Text>
           </View>
         </View>
-      )
-    }
+        <View style={styles.footer}>
+          <TouchableOpacity
+            style={[styles.button, {backgroundColor:'#000'}]}
+            onPress={() => this.goToDeckList()}>
+            <Text style={[styles.buttonText, {color: '#FFF'}]}>Go Home</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    )
   }
 }
 
